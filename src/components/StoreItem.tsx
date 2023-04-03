40824be978eeceab2067584ffcdbd1de6a45f101
raheemelassadi <raheemelassadi1@gmail.com>
2023-04-03 02:06:41 -0700

    added dynamic add to cart button

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
@@ -9,8 +9,9 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgUrl}: StoreItemProps){
+    const quantity = 0
     return ( 
-        <Card>
+        <Card className="h-100">
             <Card.Img variant="top"
                  src={imgUrl} 
                  height="200px" 
@@ -22,6 +23,11 @@ export function StoreItem({id, name, price, imgUrl}: StoreItemProps){
                     <span className="fs-2">{name}</span>
                     <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100">+ Add To Cart</Button>
+                    ) : null}
+                </div>
             </Card.Body>
         </Card>
     )
